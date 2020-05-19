c369aef6dcdfe75e79e0aa4e36354e62cc5dd386
gerardss95 <gerardsolesanz@hotmail.com>
2020-05-19 15:59:06 +0200

    update seeds

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -2,34 +2,34 @@ module.exports = [
 
 	{
 		name: 'Adidas',
-		image: "https://i.ibb.co/4sycM7y/adidas-PNG21.png",
+		image: 'https://i.ibb.co/4sycM7y/adidas-PNG21.png',
 	},
 	{
 		name: 'Nike',
-		image: "https://i.ibb.co/QMsHj6w/nike-PNG17.png",
+		image: 'https://i.ibb.co/QMsHj6w/nike-PNG17.png',
 	},
 	{
 		name: 'Vans',
-		image: "https://i.ibb.co/ZfK7bdZ/vans-logo-7830.png" ,
+		image: 'https://i.ibb.co/ZfK7bdZ/vans-logo-7830.png',
 	},
 	{
 		name: 'Reebook',
-		image: "https://i.ibb.co/Rz2y6tY/reebok-logo-5.png",
+		image: 'ttps://i.ibb.co/Rz2y6tY/reebok-logo-5.png',
 	},
 	{
 		name: 'Puma',
-		image: "https://i.ibb.co/tZ7YpXP/puma-logo-1.png",
+		image: 'https://i.ibb.co/tZ7YpXP/puma-logo-1.png',
 	},
 	{
 		name: 'New Balance',
-		image: "https://i.ibb.co/QQkTBbB/new-balance.png",
+		image: 'https://i.ibb.co/QQkTBbB/new-balance.png',
 	},
 	{
 		name: 'Converse',
-		image: "https://i.ibb.co/ypgWLPJ/converse.png",
+		image: 'https://i.ibb.co/ypgWLPJ/converse.png',
 	},
 	{
 		name: 'Asics',
-		image: "https://i.ibb.co/jV0K0Fq/asics.png",
+		image: 'https://i.ibb.co/jV0K0Fq/asics.png',
 	},
 ]
