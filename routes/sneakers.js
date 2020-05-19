46be2b7d823f9fee6e6e3d76399b73a8531f9f06
gerardss95 <gerardsolesanz@hotmail.com>
2020-05-19 19:29:56 +0200

    update

--- routes/sneakers.js
+++ routes/sneakers.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Brand = require('../models/Brand');
+//const Brand = require('../models/Brand');
 const Sneaker = require('../models/Sneaker');
 
 const router = express.Router();
@@ -7,8 +7,8 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   Sneaker.find()
   .populate('brand')
-		.then(shoes => {
-			return res.status(200).json(shoes);
+		.then(sneaker => {
+			return res.status(200).json(sneaker);
 		})
 		.catch(next);
 });
@@ -21,42 +21,42 @@ router.post('/add', (req, res, next) => {
 		info,
 		image,
 	})
-		.then(shoe => {
-			res.status(201).json(shoe);
+		.then(sneaker => {
+			res.status(201).json(sneaker);
 		})
 		.catch(next);
 });
 
 router.get('/:_id', (req, res, next) => {
-	const shoeID = req.params;
-	Sneaker.findById(shoeID)
-		.then(shoe => {
-			res.json(shoe);
+	const sneakerID = req.params;
+	Sneaker.findById(sneakerID)
+		.then(sneaker => {
+			res.json(sneaker);
 		})
 		.catch(next);
 });
 
 router.delete('/:_id', (req, res, next) => {
-	const shoeID = req.params;
-	Sneaker.findByIdAndDelete(shoeID)
-		.then(shoe => {
-			res.json(shoe);
+	const sneakerID = req.params;
+	Sneaker.findByIdAndDelete(sneakerID)
+		.then(sneaker => {
+			res.json(sneaker);
 		})
 		.catch(next);
 });
 
 router.put('/:_id', (req, res, next) => {
-	const shoeID = req.params;
+	const sneakerID = req.params;
 	const { name, brand } = req.body;
-	Sneaker.findByIdAndUpdate(shoeID, {
+	Sneaker.findByIdAndUpdate(sneakerID, {
 		name,
     brand,
     info,
     image,
 	})
-		.then(shoeUpdated => {
-			if (shoeUpdated) {
-				res.json(shoeUpdated);
+		.then(sneakerUpdated => {
+			if (sneakerUpdated) {
+				res.json(sneakerUpdated);
 			} else {
 				res.status(404).json('not found');
 			}
