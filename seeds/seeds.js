38d30391a0170da4dd1f1321e0e53dbf951f986e
gerardss95 <gerardsolesanz@hotmail.com>
2020-05-19 15:57:46 +0200

    update brands seed

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -2,38 +2,34 @@ module.exports = [
 
 	{
 		name: 'Adidas',
-		image: 'https://www.pngitem.com/pimgs/m/383-3835039_adidas-logo-transparent-background-transparent-adidas-logo-hd.png',
+		image: "https://i.ibb.co/4sycM7y/adidas-PNG21.png",
 	},
 	{
 		name: 'Nike',
-		image: 'https://www.pngitem.com/pimgs/m/383-3835039_adidas-logo-transparent-background-transparent-adidas-logo-hd.png',
-	},
-	{
-		name: 'Jordan',
-		image: 'https://www.pngitem.com/pimgs/m/383-3835039_adidas-logo-transparent-background-transparent-adidas-logo-hd.png',
+		image: "https://i.ibb.co/QMsHj6w/nike-PNG17.png",
 	},
 	{
 		name: 'Vans',
-		image: 'https://www.pngitem.com/pimgs/m/383-3835039_adidas-logo-transparent-background-transparent-adidas-logo-hd.png',
+		image: "https://i.ibb.co/ZfK7bdZ/vans-logo-7830.png" ,
 	},
 	{
 		name: 'Reebook',
-		image: 'https://www.pngitem.com/pimgs/m/383-3835039_adidas-logo-transparent-background-transparent-adidas-logo-hd.png',
+		image: "https://i.ibb.co/Rz2y6tY/reebok-logo-5.png",
 	},
 	{
 		name: 'Puma',
-		image: 'https://www.pngitem.com/pimgs/m/383-3835039_adidas-logo-transparent-background-transparent-adidas-logo-hd.png',
+		image: "https://i.ibb.co/tZ7YpXP/puma-logo-1.png",
 	},
 	{
 		name: 'New Balance',
-		image: 'https://www.pngitem.com/pimgs/m/383-3835039_adidas-logo-transparent-background-transparent-adidas-logo-hd.png',
+		image: "https://i.ibb.co/QQkTBbB/new-balance.png",
 	},
 	{
 		name: 'Converse',
-		image: 'https://www.pngitem.com/pimgs/m/383-3835039_adidas-logo-transparent-background-transparent-adidas-logo-hd.png',
+		image: "https://i.ibb.co/ypgWLPJ/converse.png",
 	},
 	{
 		name: 'Asics',
-		image: 'https://www.pngitem.com/pimgs/m/383-3835039_adidas-logo-transparent-background-transparent-adidas-logo-hd.png',
+		image: "https://i.ibb.co/jV0K0Fq/asics.png",
 	},
 ]
