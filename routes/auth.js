47f214abca0b3182424199cd3255aa7a0a9b55a7
gerardss95 <gerardsolesanz@hotmail.com>
2020-06-01 13:23:04 +0200

    validation forms

--- routes/auth.js
+++ routes/auth.js
@@ -7,10 +7,12 @@ const User = require('../models/User');
 
 const bcryptSalt = 10;
 
+const { check, validationResult } = require('express-validator');
+
 const router = express.Router();
 
 router.get('/whoami', (req, res, next) => {
-  console.log('user :>> ', req.session.currentUser);
+	console.log('user :>> ', req.session.currentUser);
 	if (req.session.currentUser) {
 		res.status(200).json(req.session.currentUser);
 	} else {
@@ -18,41 +20,59 @@ router.get('/whoami', (req, res, next) => {
 	}
 });
 
-router.post('/signup', checkUsernameAndPasswordNotEmpty, async (req, res, next) => {
-	const { username, password } = res.locals.auth;
-	try {
-		const user = await User.findOne({ username });
-		if (user) {
-			return res.status(422).json({ code: 'username-not-unique' });
+router.post(
+	'/signup',
+	[check('username').isLength({ min: 5 }), check('password').isLength({ min: 6 })],
+	async (req, res, next) => {
+		const { username, password } = res.locals.auth;
+		try {
+			const errors = validationResult(req);
+			if (!errors.isEmpty()) {
+				console.log(errors);
+				return res.status(422).json({ errors: errors.array() });
+			}
+			const user = await User.findOne({ username });
+			if (user) {
+				return res.status(422).json({ code: 'username-not-unique' });
+			}
+
+			const salt = bcrypt.genSaltSync(bcryptSalt);
+			const hashedPassword = bcrypt.hashSync(password, salt);
+
+			const newUser = await User.create({ username, hashedPassword });
+			req.session.currentUser = newUser;
+			return res.json(newUser);
+		} catch (error) {
+			next(error);
 		}
-
-		const salt = bcrypt.genSaltSync(bcryptSalt);
-		const hashedPassword = bcrypt.hashSync(password, salt);
-
-		const newUser = await User.create({ username, hashedPassword });
-		req.session.currentUser = newUser;
-		return res.json(newUser);
-	} catch (error) {
-		next(error);
 	}
-});
+);
 
-router.post('/login', checkUsernameAndPasswordNotEmpty, async (req, res, next) => {
-	const { username, password } = res.locals.auth;
-	try {
-		const user = await User.findOne({ username });
-		if (!user) {
-			return res.status(404).json({ code: 'not-found' });
+router.post(
+	'/login',
+	[check('username').isLength({ min: 5 }), check('password').isLength({ min: 6 })],
+	async (req, res, next) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			console.log(errors);
+			return res.status(422).json({ errors: errors.array() });
 		}
-		if (bcrypt.compareSync(password, user.hashedPassword)) {
-			req.session.currentUser = user;
-			return res.json(user);
+		const { username, password } = res.locals.auth;
+		try {
+			const user = await User.findOne({ username });
+			if (!user) {
+				return res.status(404).json({ code: 'Invalid Username or password' });
+			}
+			if (bcrypt.compareSync(password, user.hashedPassword)) {
+				req.session.currentUser = user;
+				return res.json(user);
+			}
+			return res.status(404).json({ code: 'Invalid Username or password' });
+		} catch (error) {
+			next(error);
 		}
-		return res.status(404).json({ code: 'not-found' });
-	} catch (error) {
-		next(error);
 	}
-});
+);
 
 router.get('/logout', (req, res, next) => {
 	req.session.destroy(err => {
